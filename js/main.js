5111ddd5d739f24c0e442c43c32aa6892613eb1e
amirbek887 <abdulazizovamirullo@gmail.com>
2023-05-05 21:14:41 +0500

    Add tabpanels section and js of it

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const elsTabLink = document.querySelectorAll('.js-tab-link');
+const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
+const elsTabsItem = document.querySelectorAll('.tabs__item');
+
+function deactivateTabItems () {
+  elsTabsItem.forEach(function (elTabsItem) {
+    elTabsItem.classList.remove('tabs__item--active');
+  });
+}
+function deactivateTabPanels () {
+  elsTabsPanel.forEach(function (elTabsPanel) {
+    elTabsPanel.classList.remove('tabpanels__item--active');
+  });
+}
+
+elsTabLink.forEach(function (elTabLink) {
+  elTabLink.addEventListener('click', function (evt) {
+    // Prevent page move
+    evt.preventDefault();
+
+    // Remove active class from tabs__item elements
+    deactivateTabItems ();
+
+    // Add active class to current tabs__item
+    elTabLink.parentElement.classList.add('tabs__item--active');
+
+    // Remove active class from tabs__panel element
+    deactivateTabPanels();
+
+    // Show activce tab panel
+    // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
+    const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
+    elTargetPanel.classList.add('tabpanels__item--active');
+  });
+});
