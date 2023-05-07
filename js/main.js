9771b83ef4e143cd164a26fad35383dfd1721ac1
amirbek887 <abdulazizovamirullo@gmail.com>
2023-05-07 08:24:57 +0500

    Refactor modifier classes into modifier object

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const modifiers = {
+  tabItemActive: 'tabs__item--active',
+  tabPanelActive: 'tabpanels__item--active',
+  accordionItemOpen: 'accordion__item--open'
+};
+
 const elsTabLink = document.querySelectorAll('.js-tab-link');
 const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
 const elsTabsItem = document.querySelectorAll('.tabs__item');
@@ -7,18 +13,18 @@ const elsAccordionItem = document.querySelectorAll('.accordion__item');
 
 function closeAccordionItems () {
   elsAccordionItem.forEach(function (elAccordionItem) {
-    elAccordionItem.classList.remove('accordion__item--open');
+    elAccordionItem.classList.remove(modifiers.accordionItemOpen);
   });
 }
 
 function deactivateTabItems () {
   elsTabsItem.forEach(function (elTabsItem) {
-    elTabsItem.classList.remove('tabs__item--active');
+    elTabsItem.classList.remove(modifiers.tabItemActive);
   });
 }
 function deactivateTabPanels () {
   elsTabsPanel.forEach(function (elTabsPanel) {
-    elTabsPanel.classList.remove('tabpanels__item--active');
+    elTabsPanel.classList.remove(modifiers.tabPanelActive);
   });
 }
 
@@ -32,7 +38,7 @@ elsTabLink.forEach(function (elTabLink) {
     deactivateTabItems ();
 
     // Add active class to current tabs__item
-    elTabLink.parentElement.classList.add('tabs__item--active');
+    elTabLink.parentElement.classList.add(modifiers.tabItemActive);
 
     // Remove active class from tabs__panel element
     deactivateTabPanels();
@@ -40,13 +46,13 @@ elsTabLink.forEach(function (elTabLink) {
     // Show activce tab panel
     // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
     const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
-    elTargetPanel.classList.add('tabpanels__item--active');
+    elTargetPanel.classList.add(modifiers.tabPanelActive);
   });
 });
 
 elsAccordionItemToggler.forEach(function (elAccordionItemToggler) {
   elAccordionItemToggler.addEventListener('click', function () {
     closeAccordionItems ();
-    elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
+    elAccordionItemToggler.closest('.accordion__item').classList.add(modifiers.accordionItemOpen);
   });
 });
